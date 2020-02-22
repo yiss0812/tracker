645d5c1aa81dd2dc8768d9a9f316d2e411479bac
yissel <contrerasjared0610@gmail.com>
2020-02-22 14:17:35 -0600

    deploy to heroku

--- routes/API.js
+++ routes/API.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout  = require("../models/workout-schema");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("./api/workout", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -21,7 +21,7 @@ router.post("/api/workout", ({ body }, res) => {
 //     });
 // });
 
-router.get("/api/workout", (req, res) => {
+router.get("./api/workout", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
     .then(dbWorkout => {
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout",{
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/API.js"));
+// app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
